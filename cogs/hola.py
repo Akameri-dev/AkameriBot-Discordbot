31b60cabebce240a0a7f66aa3bef6b1cd49ba4cb
Akameri-dev <awebo7u7123@gmail.com>
2025-09-21 16:02:17 -0400

    depuracion de errores 1

--- cogs/hola.py
+++ cogs/hola.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 class Test(commands.GroupCog, name="test"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        super().__init__()
 
     @app_commands.command(name="hola", description="Responde con un saludo")
     async def hola(self, interaction: discord.Interaction):
